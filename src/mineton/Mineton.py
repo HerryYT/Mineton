2592bff3effd0583a1c6300862444397c4846c35
HerryYT <34418030+HerryYT@users.noreply.github.com>
2020-02-11 21:39:40 +0100

    Solid binary stream base

--- src/mineton/Mineton.py
+++ src/mineton/Mineton.py
@@ -1,48 +1,60 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 from loguru import logger
-# from mineton.binarystream.BinaryStream import BinaryStream
+from mineton.binarystream.BinaryStream import BinaryStream
 import sys
 import socket
 
 # Used to recognize software version
+
 BUILD = "0.1B1"
 
 
 # Mineton main file, used to initialize the software
-# Function used to setup all software properties
-def bootstrap():
-    # Logger init
-    logger.remove()
-    logger.add(
-        sys.stderr,
-        colorize=True,
-        format="<blue>{time:HH:mm:ss}</blue> :: <level>{message}</level>"
-    )
-    logger.info("Bootstrapping Mineton v{build}...", build=BUILD)
+class Mineton:
 
-    # Socket listener
-    socket_listen(19132)
+    def __init__(self):
+        # Start all features
+        self.bootstrap()
 
+        # Socket listener
+        self.socket_listen(19132)
 
-def socket_listen(port):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind(("localhost", port))
-    logger.info("Mineton bound on localhost:{port}!", port=port)
+    # Function used to setup all software properties
+    def bootstrap(self):
+        # Logger init
+        logger.remove()
+        logger.add(
+            sys.stderr,
+            colorize=True,
+            format="<blue>{time:HH:mm:ss}</blue> :: <level>{message}</level>"
+        )
+        logger.info("Bootstrapping Mineton v{build}...", build=BUILD)
 
-    while True:
-        data, addr = sock.recvfrom(1024)
-        offline_data_handler(data=data, addr=addr)
-        # logger.debug("Received {data} from {addr}", data=data, addr=addr)
+    def socket_listen(self, port):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind(("localhost", port))
+        logger.info("Mineton bound on localhost:{port}!", port=port)
 
+        while True:
+            data, addr = sock.recvfrom(1024)
+            self.offline_data_handler(data=data, addr=addr)
+            # logger.debug("Received {data} from {addr}", data=data, addr=addr)
 
-def offline_data_handler(data, addr):
-    pid = data[0]
-    logger.debug("Received packet {pid} from {addr}", pid=hex(data[0]), addr=addr)
+    def offline_data_handler(self, data, addr):
+        pid = data[0]
+        logger.debug("Received packet {pid} from {addr}", pid=hex(data[0]), addr=addr)
 
-    if pid == 1:
-        logger.info("Got unconnected ping")
+        if pid == 1:
+            logger.info("Got unconnected ping")
+            stream = BinaryStream()
+            stream.clean()
+            stream.append(data)
+            logger.info(stream.getStream())
+            stream.getByte()
+            logger.info(stream.getLong())
+            logger.info(stream.getMagic())
 
 
 if __name__ == "__main__":
-    bootstrap()
+    Mineton()
--- src/mineton/binarystream/BinaryStream.py
+++ src/mineton/binarystream/BinaryStream.py
@@ -2,30 +2,69 @@
 # -*- coding: UTF-8 -*-
 import bitstring
 
-stream = bitstring.BitStream()
-
 
 class BinaryStream:
+    stream = bitstring.BitStream()
+    offset = 0
 
-    @staticmethod
-    def append(data):
-        if isinstance(data, BinaryStream):
-            stream.append(data.getStream())
+    def append(self, data):
+        self.stream.append(data)
+
+    def putByte(self, byte):
+        self.stream.append(f"int:8={byte}")
+
+    def getByte(self):
+        return self.stream.read(f"int:8={self.increaseOffset(1)}")
+
+    def putShort(self, short):
+        self.stream.append(f"int:16={short}")
+
+    def getShort(self):
+        return self.stream.read(f"uintbe:16={self.increaseOffset(2)}")
+
+    def putInt(self, integer):
+        self.stream.append(f"int:32={integer}")
+
+    def putLong(self, long):
+        self.stream.append(f"uintbe:32={(0xFFFFFFFF & int(long))}")
+
+    # uintbe 32 or 64? 64 seems right but is 32...
+    def getLong(self):
+        return (self.stream.read(f"uintbe:32={self.increaseOffset(4)}") << 8) + \
+               self.stream.read(f"uintbe:32={self.increaseOffset(4)}")
+
+    def putString(self, string):
+        self.stream.append(bytes(string, "utf-8"))
+
+    def getString(self):
+        print()
+
+    def putMagic(self):
+        self.stream.append(b"\x00\xff\xff\x00\xfe\xfe\xfe\xfe\xfd\xfd\xfd\xfd\x12\x34\x56\x78")
+
+    def getMagic(self):
+        # From start, to finish of Offline message length
+        return self.stream[self.offset:self.increaseOffset(16, True)]
+
+    def getBool(self):
+        return self.getByte() != 0
+
+    def putBool(self, boolean):
+        if boolean:
+            self.putByte(1)
         else:
-            stream.append(data)
+            self.putByte(0)
 
-    @staticmethod
-    def putByte(byte):
-        stream.append(f"int:8={byte}")
+    def increaseOffset(self, v, ret=False):
+        self.offset += v
+        if ret:
+            return self.offset
+        else:
+            return self.offset-v
 
-    @staticmethod
-    def putShort(short):
-        stream.append(f"int:16={short}")
+    def clean(self):
+        self.stream = bitstring.BitStream()
+        self.offset = 0
 
-    @staticmethod
-    def putInt(integer):
-        stream.append(f"int:32={integer}")
-
-    @staticmethod
-    def getStream():
-        return stream
+    def getStream(self):
+        return self.stream
